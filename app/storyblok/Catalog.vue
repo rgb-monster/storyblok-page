a1d8cc4c1f4e64d8ad2f09c782ad60f423ed64cb
Tom Striker <toms.baugis@gmail.com>
2026-05-20 17:38:54 +0200

    wip-ing

---
+++ app/storyblok/Catalog.vue
@@ -0,0 +1,28 @@
+<script>
+    import chroma from "chroma-js";
+
+    export default {
+        props: {
+            blok: Object,
+        },
+        data() {
+            return {};
+        },
+        computed: {
+            dateFrom: state => (state.blok.date_from || "").split(" ")[0],
+            dateTo: state => (state.blok.date_to || "").split(" ")[0],
+        },
+    };
+</script>
+
+<template>
+    <div class="show-catalog" v-editable="blok">
+        {{ dateFrom }} {{ dateTo }}
+    </div>
+</template>
+
+<style lang="css">
+    .show-catalog {
+        padding: 50px;
+    }
+</style>
