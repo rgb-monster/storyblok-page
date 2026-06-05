bb9945517bd998ecabf260b93caf073780128d44
Tom Striker <toms.baugis@gmail.com>
2026-06-05 17:35:33 +0200

    add image element

---
+++ app/storyblok/Image.vue
@@ -0,0 +1,7 @@
+<script setup>
+    defineProps({blok: Object});
+</script>
+
+<template>
+    <img class="section-image" v-editable="blok" :src="blok.image?.filename" :alt="blok.image?.alt" />
+</template>
